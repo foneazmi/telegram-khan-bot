8e70a18b3c1e7f2bae6eabb9a1ed0f6c82d49d76
Farkhan Azmi <ghost@Farkhans-MacBook-Pro.local>
2022-01-02 20:26:56 +0700

    fix

--- src/app.js
+++ src/app.js
@@ -53,6 +53,6 @@ app.post("/webhook", (req, res) => {
 
 app.post("/log", (req, res) => {
   let { query, body } = req;
-  bot.sendMessage(query.chatId, body);
+  bot.sendMessage(query.chatId, body.log);
   res.sendStatus(200);
 });
